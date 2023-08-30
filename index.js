a64e90f591255675e1704cebb04a7583d054bce7
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-08-30 15:48:03 -0700

    playGame function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+let result;
 
 function getComputerChoice() {
   let computerChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -8,31 +11,59 @@ function getComputerChoice() {
 function playRound() {
   let computerSelection = getComputerChoice();
   let playerSelection = prompt("Rock, Paper, or Scissors").toLowerCase();
-  let result;
   if (computerSelection === playerSelection) {
     result = "Tie";
   } else if (playerSelection === "rock") {
     if (computerSelection === "paper") {
       result = "Computer Won";
+      computerScore++;
     } else {
       result = "Player Won";
+      playerScore++;
     }
   } else if (playerSelection === "paper") {
     if (computerSelection === "scissors") {
       result = "Computer Won";
+      computerScore++;
     } else {
       result = "Player Won";
+      playerScore++;
     }
   } else if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
       result = "Computer Won";
+      computerScore++;
     } else {
       result = "Player Won";
+      playerScore++;
     }
   }
   console.log(`Computer chose ${computerSelection}`);
   console.log(`Player chose ${playerSelection}`);
+  console.log(result);
   return result;
 }
 
-console.log(playRound());
+function playGame() {
+  let round = 1;
+  while (round <= 5) {
+    console.log(`Round: ${round}`);
+    playRound();
+    round++;
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+  }
+  if (playerScore > computerScore) {
+    console.log("Player Wins the Game!!!");
+  } else if (computerScore > playerScore) {
+    console.log("Computer Wins the Game!!!");
+  } else {
+    console.log("The Game is a Tie!!!");
+  }
+  let playAgain = prompt("Play Again? (Y/N)");
+  if (playAgain === "Y") {
+    playGame();
+  }
+}
+
+console.log(playGame());
